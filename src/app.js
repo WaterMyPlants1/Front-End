9ac6c2f881469ff0d5d053a17b4f11c8cd652a3d
Michael Enslein <michael_enslein@Michaels-MacBook-Pro.local>
2021-12-23 12:00:00 -0500

    trying to clean bugs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import React, {Switch} from "react";
-import {Route} from 'react-router-dom';
+import React, { Switch, useEffect, useState } from "react";
+import { Route } from 'react-router-dom';
 
 import { PrivateRoute } from "./components/PrivateRoute";
 import PlantsList from "./components/PlantsList";
@@ -7,23 +7,25 @@ import Plant from "./components/plant";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
+import * as yup from 'yup';
+
 
 function App() {
     return (
       <div className='App'>
         <Switch>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/signup'>
-          <Signup />
-        </Route>
-        <PrivateRoute path='/plant'>
-          <Plant />
-        </PrivateRoute>
-        <PrivateRoute path='/plant/list'>
-          <PlantsList />
-        </PrivateRoute>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+          <PrivateRoute path='/plant'>
+            <Plant />
+          </PrivateRoute>
+          <PrivateRoute path='/plant/list'>
+            <PlantsList />
+          </PrivateRoute>
         </Switch>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
